36088826d5323cd6ec108d44fe7cc3af6aa8e4e9
Igor Minayeu <igor.minayeu@gmail.com>
2026-01-26 21:08:08 +0300

    extending properties

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <div>
             <Box>
                 <StyledBtn>Button</StyledBtn>
+                <SuperButton>Button2</SuperButton>
             </Box>
         </div>
     )
@@ -19,11 +20,16 @@ const StyledBtn = styled.button`
     border: none;
     background-color: #ef319b;
     padding: 10px 20px;
-    border-radius: 5px;
     color: snow;
     font-size: 2rem;
     font-weight: bold;
 
+`
+
+const SuperButton=styled(StyledBtn)`
+    border-radius: 5px;
+    background-color: #f4f002;
+    color: #434040;
 `
 
 const Box = styled.div`
