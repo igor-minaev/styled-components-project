c2a54c6d654177bde59e66432240edf855012412
Igor Minayeu <igor.minayeu@gmail.com>
2026-01-27 19:42:50 +0300

    Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ function App() {
 
     return (
         <div>
+            <Menu>
+                <ul>
+                    <li><a href="">menu item 1</a></li>
+                    <li><a href="">menu item 2</a></li>
+                    <li><a href="">menu item 3</a></li>
+                </ul>
+            </Menu>
             <Box>
                 <StyledBtn as={Link} href={"#"}>LinkComponent</StyledBtn>
                 <StyledBtn as={"a"} href={"#"}>Link</StyledBtn>
@@ -26,12 +33,20 @@ const StyledBtn = styled.button`
     font-size: 2rem;
     font-weight: bold;
 
+    &:hover {
+        background-color: #700241;
+    }
+
+    &:last-child {
+        background-color: #d240ec;
+    }
 `
 
 const Link = styled.a`
     color: #328eca;
     font-size: 2rem;
     font-weight: bold;
+    background-color: transparent;
 
 `
 
@@ -46,5 +61,20 @@ const Box = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 20px;
 
+    button {
+        cursor: pointer;
+    }
+
+    ${Link} {
+        cursor: zoom-in;
+    }
+`
+
+const Menu=styled.nav `
+    ul{
+        list-style: none;
+        padding: 0;
+    }
 `
